aef1b3df46fda4b2eb105c213f42f2d5727503ee
Ionut Nicolae <nicolaeionut0904@gmail.com>
2024-04-10 19:35:08 +0300

    Added solution for ex6 and ex5
    Signed-off-by: Ionut Nicolae <nicolaeionut0904@gmail.com>

--- laborator/content/rolul-registrelor-adresare/5-6-7-sum-array/sum-array.asm
+++ laborator/content/rolul-registrelor-adresare/5-6-7-sum-array/sum-array.asm
@@ -27,6 +27,27 @@ add_byte_array_element:
 
 
     ; TODO: Compute sum for elements in word_array and dword_array.
+    mov ecx, ARRAY_SIZE
+    xor eax, eax
+    xor edx, edx
+
+add_word_array_element:
+    mov dx, [word_array + 2 * (ecx - 1)]
+    add eax, edx
+    loop add_word_array_element
+
+    PRINTF32 `Array Word sum is %u\n\x0`, eax
+
+    mov ecx, ARRAY_SIZE
+    xor eax, eax
+    xor edx, edx
+
+add_dword_array_element:
+    mov edx, [dword_array + 4 * (ecx - 1)]
+    add eax, edx
+    loop add_dword_array_element
+
+    PRINTF32 `Array DWord sum is %u\n\x0`, eax
 
     leave
     ret
