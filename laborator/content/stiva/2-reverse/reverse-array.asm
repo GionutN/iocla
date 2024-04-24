a74729eb977915483acbb9903da9cdf04ddf37f7
Ionut Nicolae <nicolaeionut0904@gmail.com>
2024-04-24 19:35:26 +0300

    Added solution for ex2 using pop, push and a loop
    Signed-off-by: Ionut Nicolae <nicolaeionut0904@gmail.com>

--- laborator/content/stiva/2-reverse/reverse-array.asm
+++ laborator/content/stiva/2-reverse/reverse-array.asm
@@ -14,7 +14,20 @@ global main
 main:
 
     ; TODO push the elements of the array on the stack
+    xor ecx, ecx
+push_array:
+    push dword [input + 4 * ecx]
+    inc ecx
+    cmp ecx, ARRAY_LEN
+    jb push_array
+
     ; TODO retrieve the elements (pop) from the stack into the output array
+    xor ecx, ecx
+pop_array:
+    pop dword [output + 4 * ecx]
+    inc ecx
+    cmp ecx, ARRAY_LEN
+    jb pop_array
 
     PRINTF32 `Reversed array: \n\x0`
     xor ecx, ecx
