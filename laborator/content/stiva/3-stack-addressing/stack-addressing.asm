2a6375288b8308e4d361346f26fde8cb134c0fe8
Ionut Nicolae <nicolaeionut0904@gmail.com>
2024-04-24 20:25:39 +0300

    Added solution for ex3, maybe broken esp??
    Signed-off-by: Ionut Nicolae <nicolaeionut0904@gmail.com>

--- laborator/content/stiva/3-stack-addressing/stack-addressing.asm
+++ laborator/content/stiva/3-stack-addressing/stack-addressing.asm
@@ -1,7 +1,7 @@
 %include "../utils/printf32.asm"
 
 %define NUM 5
-   
+
 section .text
 
 extern printf
@@ -25,10 +25,32 @@ push_nums:
 
     ; TODO 2: print the stack in "address: value" format in the range of [ESP:EBP]
     ; use PRINTF32 macro - see format above
+    mov ecx, esp
+inspect_stack:
+    PRINTF32 `%x: %hhu\n\x0`, ecx, byte [ecx]
+    inc ecx
+    cmp ecx, ebp
+    jb inspect_stack
 
     ; TODO 3: print the string
+    mov ecx, esp
+print_string:
+    PRINTF32 `%c\x0`, byte [ecx]
+    inc ecx
+    cmp byte [ecx], 0
+    ja print_string
 
+    PRINTF32 `\n\x0`
     ; TODO 4: print the array on the stack, element by element.
+    mov esp, ebp
+    sub esp, 5
+    xor ecx, ecx
+    PRINTF32 `%x\n\x0`, esp
+print_array:
+    PRINTF32 `%d \x0`, dword [esp + 4 * ecx]
+    inc ecx
+    cmp ecx, NUM
+    jb print_array
 
     ; restore the previous value of the EBP (Base Pointer)
     mov esp, ebp
